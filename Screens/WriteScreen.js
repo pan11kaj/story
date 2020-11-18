17e5bb7f35e395e78535c358b1c4661b01f949fa
pan11kaj <68064022+pan11kaj@users.noreply.github.com>
2020-11-18 02:57:14 -0800

    Add files via upload

---
+++ Screens/WriteScreen.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { StyleSheet, Text, View,TextInput ,TouchableOpacity} from 'react-native';
+
+export default class WriteScreen extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            storyTitle :'',writting:'',author:''
+        }
+    }
+ render(){
+     return(
+         <View>
+         <TextInput
+         style={style.inputs}
+         placeholder="Story Title"
+         value={this.state.storyTitle}
+         />
+    <TextInput
+         style={style.inputs}
+         placeholder="Author"
+         value={this.state.author}
+         />
+             <TextInput
+         style={style.input2}
+         placeholder="Write your stroy"
+         value={this.state.writting}
+         />
+         <TouchableOpacity style={{backgroundColor:'red',width:80,justifyContent:'center'}}><Text style={{color:'white',textAlign:'center'}}>Submit</Text></TouchableOpacity>
+         </View>
+     )
+ }   
+}
+const style = StyleSheet.create({
+  inputs:{
+ justifyContent:'center',
+width:140,
+borderWidth:2,borderColor:'blue'
+  },
+  input2:{
+    justifyContent:'center',
+    width:100,
+    height:200,
+    borderWidth:2
+  }  
+})
